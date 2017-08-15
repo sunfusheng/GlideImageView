d9da035418c890d2189a22e99cb6ba53b6928840
sfsheng0322 <sfsheng0322@gmail.com>
2017-08-15 14:52:40 +0800

    update glide version

--- GlideImageView/src/main/java/com/sunfusheng/glideimageview/progress/ProgressAppGlideModule.java
+++ GlideImageView/src/main/java/com/sunfusheng/glideimageview/progress/ProgressAppGlideModule.java
@@ -2,6 +2,7 @@ package com.sunfusheng.glideimageview.progress;
 
 import android.content.Context;
 
+import com.bumptech.glide.Glide;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
@@ -15,9 +16,10 @@ import java.io.InputStream;
  */
 @GlideModule
 public class ProgressAppGlideModule extends AppGlideModule {
-
     @Override
-    public void registerComponents(Context context, Registry registry) {
+    public void registerComponents(Context context, Glide glide, Registry registry) {
+        super.registerComponents(context, glide, registry);
         registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(ProgressManager.getOkHttpClient()));
     }
+
 }
