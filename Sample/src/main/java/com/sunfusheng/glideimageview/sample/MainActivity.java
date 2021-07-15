138876da6a424b6c72de7a834e4dd059e854532c
sunfusheng001 <sunfusheng001@ke.com>
2021-07-15 19:05:11 +0800

    update

--- Sample/src/main/java/com/sunfusheng/glideimageview/sample/MainActivity.java
+++ Sample/src/main/java/com/sunfusheng/glideimageview/sample/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.sunfusheng.FirUpdater;
 import com.sunfusheng.GlideImageView;
 import com.sunfusheng.glideimageview.sample.about.AboutActivity;
 import com.sunfusheng.glideimageview.sample.image.SingleImageActivity;
@@ -21,149 +20,169 @@ import com.sunfusheng.transformation.BlurTransformation;
  */
 public class MainActivity extends BaseActivity {
 
-    GlideImageView image11;
-    GlideImageView image12;
-    GlideImageView image13;
-    GlideImageView image14;
+  GlideImageView image11;
+  GlideImageView image12;
+  GlideImageView image13;
+  GlideImageView image14;
 
-    GlideImageView image21;
-    GlideImageView image22;
-    GlideImageView image23;
-    GlideImageView image24;
+  GlideImageView image21;
+  GlideImageView image22;
+  GlideImageView image23;
+  GlideImageView image24;
 
-    GlideImageView image31;
-    CircleProgressView progressView1;
-    GlideImageView image32;
-    CircleProgressView progressView2;
+  GlideImageView image31;
+  CircleProgressView progressView1;
+  GlideImageView image32;
+  CircleProgressView progressView2;
 
-    GlideImageView image41;
+  GlideImageView image41;
 
-    View draggableView;
+  View draggableView;
 
-    String url1 = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1497688355699&di=ea69a930b82ce88561c635089995e124&imgtype=0&src=http%3A%2F%2Fcms-bucket.nosdn.127.net%2Ff84e566bcf654b3698363409fbd676ef20161119091503.jpg";
-    String url2 = "http://img1.imgtn.bdimg.com/it/u=4027212837,1228313366&fm=23&gp=0.jpg";
-    String url3 = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1529402445474&di=b5da3b2f6a466e618e1e32d4dd2bda4d&imgtype=0&src=http%3A%2F%2F2b.zol-img.com.cn%2Fproduct%2F133_500x2000%2F801%2Fce21ke76FRh4A.jpg";
+  String url1 =
+      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1497688355699&di=ea69a930b82ce88561c635089995e124&imgtype=0&src=http%3A%2F%2Fcms-bucket.nosdn.127.net%2Ff84e566bcf654b3698363409fbd676ef20161119091503.jpg";
+  String url2 = "http://img1.imgtn.bdimg.com/it/u=4027212837,1228313366&fm=23&gp=0.jpg";
+  String url3 =
+      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1529402445474&di=b5da3b2f6a466e618e1e32d4dd2bda4d&imgtype=0&src=http%3A%2F%2F2b.zol-img.com.cn%2Fproduct%2F133_500x2000%2F801%2Fce21ke76FRh4A.jpg";
 
-    String gif1 = "http://img.zcool.cn/community/01e97857c929630000012e7e3c2acf.gif";
-    String gif2 = "http://5b0988e595225.cdn.sohucs.com/images/20171202/a1cc52d5522f48a8a2d6e7426b13f82b.gif";
-    String gif3 = "http://img.zcool.cn/community/01d6dd554b93f0000001bf72b4f6ec.jpg";
+  String gif1 = "http://img.zcool.cn/community/01e97857c929630000012e7e3c2acf.gif";
+  String gif2 =
+      "http://5b0988e595225.cdn.sohucs.com/images/20171202/a1cc52d5522f48a8a2d6e7426b13f82b.gif";
+  String gif3 = "http://img.zcool.cn/community/01d6dd554b93f0000001bf72b4f6ec.jpg";
 
-    public static final String cat = "https://raw.githubusercontent.com/sfsheng0322/GlideImageView/master/resources/cat.jpg";
-    public static final String cat_thumbnail = "https://raw.githubusercontent.com/sfsheng0322/GlideImageView/master/resources/cat_thumbnail.jpg";
+  public static final String cat =
+      "https://raw.githubusercontent.com/sfsheng0322/GlideImageView/master/resources/cat.jpg";
+  public static final String cat_thumbnail =
+      "https://raw.githubusercontent.com/sfsheng0322/GlideImageView/master/resources/cat_thumbnail.jpg";
 
-    public static final String girl = "https://raw.githubusercontent.com/sfsheng0322/GlideImageView/master/resources/girl.jpg";
-    public static final String girl_thumbnail = "https://raw.githubusercontent.com/sfsheng0322/GlideImageView/master/resources/girl_thumbnail.jpg";
+  public static final String girl =
+      "https://raw.githubusercontent.com/sfsheng0322/GlideImageView/master/resources/girl.jpg";
+  public static final String girl_thumbnail =
+      "https://raw.githubusercontent.com/sfsheng0322/GlideImageView/master/resources/girl_thumbnail.jpg";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        new FirUpdater(this, "3c57fb226edf7facf821501e4eba08d2", "59461f4c548b7a45cb000050").checkVersion();
+    //new FirUpdater(this, "3c57fb226edf7facf821501e4eba08d2", "59461f4c548b7a45cb000050").checkVersion();
 
-        image11 = findViewById(R.id.image11);
-        image12 = findViewById(R.id.image12);
-        image13 = findViewById(R.id.image13);
-        image14 = findViewById(R.id.image14);
+    image11 = findViewById(R.id.image11);
+    image12 = findViewById(R.id.image12);
+    image13 = findViewById(R.id.image13);
+    image14 = findViewById(R.id.image14);
 
-        image21 = findViewById(R.id.image21);
-        image22 = findViewById(R.id.image22);
-        image23 = findViewById(R.id.image23);
-        image24 = findViewById(R.id.image24);
+    image21 = findViewById(R.id.image21);
+    image22 = findViewById(R.id.image22);
+    image23 = findViewById(R.id.image23);
+    image24 = findViewById(R.id.image24);
 
-        image31 = findViewById(R.id.image31);
-        progressView1 = findViewById(R.id.progressView1);
-        image32 = findViewById(R.id.image32);
-        progressView2 = findViewById(R.id.progressView2);
+    image31 = findViewById(R.id.image31);
+    progressView1 = findViewById(R.id.progressView1);
+    image32 = findViewById(R.id.image32);
+    progressView2 = findViewById(R.id.progressView2);
 
-        image41 = findViewById(R.id.image41);
+    image41 = findViewById(R.id.image41);
 
-        draggableView = findViewById(R.id.draggableView);
-        image11.setOnClickListener(v -> startActivity(new Intent(mContext, RecyclerViewActivity.class)));
-        draggableView.setOnClickListener(v -> startActivity(new Intent(mContext, NineImageViewActivity.class)));
+    draggableView = findViewById(R.id.draggableView);
+    image11.setOnClickListener(
+        v -> startActivity(new Intent(mContext, RecyclerViewActivity.class)));
+    draggableView.setOnClickListener(
+        v -> startActivity(new Intent(mContext, NineImageViewActivity.class)));
 
-        image31.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, SingleImageActivity.class);
-            intent.putExtra(SingleImageActivity.KEY_IMAGE_URL, girl);
-            intent.putExtra(SingleImageActivity.KEY_IMAGE_URL_THUMBNAIL, girl_thumbnail);
-            ActivityOptionsCompat compat = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this, image31, getString(R.string.transitional_image));
-            ActivityCompat.startActivity(MainActivity.this, intent, compat.toBundle());
+    image31.setOnClickListener(v -> {
+      Intent intent = new Intent(MainActivity.this, SingleImageActivity.class);
+      intent.putExtra(SingleImageActivity.KEY_IMAGE_URL, girl);
+      intent.putExtra(SingleImageActivity.KEY_IMAGE_URL_THUMBNAIL, girl_thumbnail);
+      ActivityOptionsCompat compat =
+          ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this, image31,
+              getString(R.string.transitional_image));
+      ActivityCompat.startActivity(MainActivity.this, intent, compat.toBundle());
+    });
+
+    image32.setOnClickListener(v -> {
+      Intent intent = new Intent(MainActivity.this, SingleImageActivity.class);
+      intent.putExtra(SingleImageActivity.KEY_IMAGE_URL, cat);
+      intent.putExtra(SingleImageActivity.KEY_IMAGE_URL_THUMBNAIL, cat_thumbnail);
+      ActivityOptionsCompat compat =
+          ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this, image32,
+              getString(R.string.transitional_image));
+      ActivityCompat.startActivity(MainActivity.this, intent, compat.toBundle());
+    });
+
+    line1();
+    line2();
+    line3();
+    line4();
+  }
+
+  private void line1() {
+    image11.enableState(true).load(url1);
+    image12.loadCircle(url1);
+    image13.load(url2, R.mipmap.image_loading);
+    image14.load(url2, R.mipmap.image_loading, 10);
+  }
+
+  private void line2() {
+    image21.fitCenter().load(gif2, R.mipmap.image_loading, 10);
+    image22.fitCenter().load(gif1);
+    image23.fitCenter().loadCircle(gif3);
+    image24.fitCenter().loadDrawable(R.drawable.gif_robot_walk);
+  }
+
+  private void line3() {
+    image31.centerCrop()
+        .error(R.mipmap.image_load_err)
+        .diskCacheStrategy(DiskCacheStrategy.NONE)
+        .load(girl, R.color.placeholder, (isComplete, percentage, bytesRead, totalBytes) -> {
+          //            Log.d("--->", "load percentage: " + percentage + " totalBytes: " + totalBytes + " bytesRead: " + bytesRead);
+          if (isComplete) {
+            progressView1.setVisibility(View.GONE);
+          } else {
+            progressView1.setVisibility(View.VISIBLE);
+            progressView1.setProgress(percentage);
+          }
         });
 
-        image32.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, SingleImageActivity.class);
-            intent.putExtra(SingleImageActivity.KEY_IMAGE_URL, cat);
-            intent.putExtra(SingleImageActivity.KEY_IMAGE_URL_THUMBNAIL, cat_thumbnail);
-            ActivityOptionsCompat compat = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this, image32, getString(R.string.transitional_image));
-            ActivityCompat.startActivity(MainActivity.this, intent, compat.toBundle());
+    image32.centerCrop()
+        .error(R.mipmap.image_load_err)
+        .load(cat, R.color.placeholder, (isComplete, percentage, bytesRead, totalBytes) -> {
+          if (isComplete) {
+            progressView2.setVisibility(View.GONE);
+          } else {
+            progressView2.setVisibility(View.VISIBLE);
+            progressView2.setProgress(percentage);
+          }
         });
+  }
 
-        line1();
-        line2();
-        line3();
-        line4();
-    }
+  private void line4() {
+    image41.fitCenter().load(url3, R.mipmap.image_loading, new BlurTransformation(this, 25, 1));
 
-    private void line1() {
-        image11.enableState(true).load(url1);
-        image12.loadCircle(url1);
-        image13.load(url2, R.mipmap.image_loading);
-        image14.load(url2, R.mipmap.image_loading, 10);
-    }
+    image41.setOnClickListener(v -> {
+      Intent intent = new Intent(MainActivity.this, SingleImageActivity.class);
+      intent.putExtra(SingleImageActivity.KEY_IMAGE_URL, url3);
+      ActivityOptionsCompat compat =
+          ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this, image41,
+              getString(R.string.transitional_image));
+      ActivityCompat.startActivity(MainActivity.this, intent, compat.toBundle());
+    });
+  }
 
-    private void line2() {
-        image21.fitCenter().load(gif2, R.mipmap.image_loading, 10);
-        image22.fitCenter().load(gif1);
-        image23.fitCenter().loadCircle(gif3);
-        image24.fitCenter().loadDrawable(R.drawable.gif_robot_walk);
-    }
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
 
-    private void line3() {
-        image31.centerCrop().error(R.mipmap.image_load_err).diskCacheStrategy(DiskCacheStrategy.NONE).load(girl, R.color.placeholder, (isComplete, percentage, bytesRead, totalBytes) -> {
-//            Log.d("--->", "load percentage: " + percentage + " totalBytes: " + totalBytes + " bytesRead: " + bytesRead);
-            if (isComplete) {
-                progressView1.setVisibility(View.GONE);
-            } else {
-                progressView1.setVisibility(View.VISIBLE);
-                progressView1.setProgress(percentage);
-            }
-        });
-
-        image32.centerCrop().error(R.mipmap.image_load_err).load(cat, R.color.placeholder, (isComplete, percentage, bytesRead, totalBytes) -> {
-            if (isComplete) {
-                progressView2.setVisibility(View.GONE);
-            } else {
-                progressView2.setVisibility(View.VISIBLE);
-                progressView2.setProgress(percentage);
-            }
-        });
-    }
-
-    private void line4() {
-        image41.fitCenter().load(url3, R.mipmap.image_loading, new BlurTransformation(this, 25, 1));
-
-        image41.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, SingleImageActivity.class);
-            intent.putExtra(SingleImageActivity.KEY_IMAGE_URL, url3);
-            ActivityOptionsCompat compat = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this, image41, getString(R.string.transitional_image));
-            ActivityCompat.startActivity(MainActivity.this, intent, compat.toBundle());
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.item_menu_app:
+        startActivity(new Intent(this, AboutActivity.class));
         return true;
+      default:
+        return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.item_menu_app:
-                startActivity(new Intent(this, AboutActivity.class));
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+  }
 }
--- Sample/src/main/java/com/sunfusheng/glideimageview/sample/widget/DraggableFrameLayout.java
+++ Sample/src/main/java/com/sunfusheng/glideimageview/sample/widget/DraggableFrameLayout.java
@@ -18,113 +18,114 @@ import com.sunfusheng.util.Utils;
  */
 public class DraggableFrameLayout extends FrameLayout {
 
-    private ViewDragHelper dragHelper;
-    private int screenWidth;
-    private int margin = 10;
+  private ViewDragHelper dragHelper;
+  private int screenWidth;
+  private int margin = 10;
 
-    public DraggableFrameLayout(@NonNull Context context) {
-        this(context, null);
-    }
+  public DraggableFrameLayout(@NonNull Context context) {
+    this(context, null);
+  }
 
-    public DraggableFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
+  public DraggableFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+    this(context, attrs, 0);
+  }
 
-    public DraggableFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
-    }
+  public DraggableFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs,
+      @AttrRes int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init();
+  }
 
-    private void init() {
-        screenWidth = Utils.getWindowWidth(getContext());
-        margin = Utils.dp2px(getContext(), margin);
+  private void init() {
+    screenWidth = Utils.getWindowWidth(getContext());
+    margin = Utils.dp2px(getContext(), margin);
 
-        dragHelper = ViewDragHelper.create(this, new ViewDragHelper.Callback() {
+    dragHelper = ViewDragHelper.create(this, new ViewDragHelper.Callback() {
 
-            @Override
-            public boolean tryCaptureView(@NonNull View child, int pointerId) {
-                return true;
-            }
+      @Override
+      public boolean tryCaptureView(@NonNull View child, int pointerId) {
+        return true;
+      }
 
-            @Override
-            public void onViewCaptured(@NonNull View capturedChild, int activePointerId) {
-                super.onViewCaptured(capturedChild, activePointerId);
-            }
+      @Override
+      public void onViewCaptured(@NonNull View capturedChild, int activePointerId) {
+        super.onViewCaptured(capturedChild, activePointerId);
+      }
 
-            @Override
-            public int clampViewPositionHorizontal(@NonNull View child, int left, int dx) {
-                return left;
-            }
+      @Override
+      public int clampViewPositionHorizontal(@NonNull View child, int left, int dx) {
+        return left;
+      }
 
-            @Override
-            public int clampViewPositionVertical(@NonNull View child, int top, int dy) {
-                return top;
-            }
+      @Override
+      public int clampViewPositionVertical(@NonNull View child, int top, int dy) {
+        return top;
+      }
 
-            @Override
-            public int getViewHorizontalDragRange(@NonNull View child) {
-                return getMeasuredWidth() - child.getMeasuredWidth();
-            }
+      @Override
+      public int getViewHorizontalDragRange(@NonNull View child) {
+        return getMeasuredWidth() - child.getMeasuredWidth();
+      }
 
-            @Override
-            public int getViewVerticalDragRange(@NonNull View child) {
-                return getMeasuredHeight() - child.getMeasuredHeight();
-            }
+      @Override
+      public int getViewVerticalDragRange(@NonNull View child) {
+        return getMeasuredHeight() - child.getMeasuredHeight();
+      }
 
-            @Override
-            public void onViewReleased(@NonNull View releasedChild, float xvel, float yvel) {
-                super.onViewReleased(releasedChild, xvel, yvel);
-                int viewWidth = releasedChild.getWidth();
-                int viewHeight = releasedChild.getHeight();
-                int curLeft = releasedChild.getLeft();
-                int curTop = releasedChild.getTop();
+      @Override
+      public void onViewReleased(@NonNull View releasedChild, float xvel, float yvel) {
+        super.onViewReleased(releasedChild, xvel, yvel);
+        int viewWidth = releasedChild.getWidth();
+        int viewHeight = releasedChild.getHeight();
+        int curLeft = releasedChild.getLeft();
+        int curTop = releasedChild.getTop();
 
-                int finalLeft = margin;
-                int finalTop = curTop < margin ? margin : curTop;
+        int finalLeft = margin;
+        int finalTop = Math.max(curTop, margin);
 
-                if ((curLeft + viewWidth / 2) > screenWidth / 2) {
-                    finalLeft = screenWidth - viewWidth - margin;
-                }
-
-                if ((finalTop + viewHeight) > getHeight()) {
-                    finalTop = getHeight() - viewHeight - margin;
-                }
-
-                dragHelper.settleCapturedViewAt(finalLeft, finalTop);
-                invalidate();
-            }
-        });
-    }
-
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        return dragHelper.shouldInterceptTouchEvent(ev);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        dragHelper.processTouchEvent(event);
-        return isTouchChildView(event);
-    }
-
-    @Override
-    public void computeScroll() {
-        super.computeScroll();
-        if (dragHelper.continueSettling(true)) {
-            invalidate();
+        if ((curLeft + viewWidth / 2) > screenWidth / 2) {
+          finalLeft = screenWidth - viewWidth - margin;
         }
-    }
 
-    private boolean isTouchChildView(MotionEvent ev) {
-        Rect rect = new Rect();
-        for (int i = 0; i < getChildCount(); i++) {
-            View view = getChildAt(i);
-            rect.set((int) view.getX(), (int) view.getY(), (int) view.getX() + view.getWidth(), (int) view.getY() + view.getHeight());
-            if (rect.contains((int) ev.getX(), (int) ev.getY())) {
-                return true;
-            }
+        if ((finalTop + viewHeight) > getHeight()) {
+          finalTop = getHeight() - viewHeight - margin;
         }
-        return false;
-    }
 
+        dragHelper.settleCapturedViewAt(finalLeft, finalTop);
+        invalidate();
+      }
+    });
+  }
+
+  @Override
+  public boolean onInterceptTouchEvent(MotionEvent ev) {
+    return dragHelper.shouldInterceptTouchEvent(ev);
+  }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+    dragHelper.processTouchEvent(event);
+    return isTouchChildView(event);
+  }
+
+  @Override
+  public void computeScroll() {
+    super.computeScroll();
+    if (dragHelper.continueSettling(true)) {
+      invalidate();
+    }
+  }
+
+  private boolean isTouchChildView(MotionEvent ev) {
+    Rect rect = new Rect();
+    for (int i = 0; i < getChildCount(); i++) {
+      View view = getChildAt(i);
+      rect.set((int) view.getX(), (int) view.getY(), (int) view.getX() + view.getWidth(),
+          (int) view.getY() + view.getHeight());
+      if (rect.contains((int) ev.getX(), (int) ev.getY())) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
